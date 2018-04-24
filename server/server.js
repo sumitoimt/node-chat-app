97fae41c26da5595fdb9c48e5bcf828fbebae494
sumitoimt <sumitoimt@gmail.com>
2018-04-24 13:27:30 +0530

    Init Commit

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-	console.log("staert");
+app.listen(port,()=>{
+	console.log(`Server is up on ${port}`);
 })
