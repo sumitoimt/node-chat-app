a68e8e3345e787ba964120361bcf210ec3be52e9
sumitoimt <sumitoimt@gmail.com>
2018-04-24 17:37:05 +0530

    Add and create message event

--- server/server.js
+++ server/server.js
@@ -3,20 +3,39 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+
 const publicPath = path.join(__dirname,'../public');
+
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io= socketIO(server);
 
+
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
  console.log('New user Connected');
+
+ socket.emit('newMessage',{
+ 	from:'donaldsmith',
+ 	text:'Hey what is going on.',
+ 	createdAt:12321
+ });
+
+ socket.on('createMessage',(message)=>{
+ 	console.log('createMessage',message);
+ })
+
  socket.on('disconnect',()=>{
  console.log('user Disconnected');
 
  });
+
+ socket.on('createEmail',(newEmail)=>{
+ 	console.log('createEmail',newEmail);
+ });
+
 });
 
 server.listen(port,()=>{
