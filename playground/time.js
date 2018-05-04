de226c222140a559d7b4e38575be8422e1f27e28
sumitoimt <sumitoimt@gmail.com>
2018-05-04 17:27:59 +0530

    style

---
+++ playground/time.js
@@ -0,0 +1,11 @@
+var moment = require('moment');
+/*var date = new Date();
+var months = ['Jan','Feb'];
+console.log(date.getMonth());*/
+/*var date = moment();
+date.add(100,'year').subtract(9,'months');
+console.log(date.format('MMM Do, YYYY'));*/
+new Date().getTime();
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
