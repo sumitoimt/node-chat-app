f97b74315a4f51c74fefdd653080e5899bb2d252
sumitoimt <sumitoimt@gmail.com>
2018-05-04 17:52:50 +0530

    Format Time stamp using moment js

--- playground/time.js
+++ playground/time.js
@@ -5,7 +5,8 @@ console.log(date.getMonth());*/
 /*var date = moment();
 date.add(100,'year').subtract(9,'months');
 console.log(date.format('MMM Do, YYYY'));*/
-new Date().getTime();
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp)
 var createdAt = 1234;
 var date = moment(createdAt);
 console.log(date.format('h:mm a'))
